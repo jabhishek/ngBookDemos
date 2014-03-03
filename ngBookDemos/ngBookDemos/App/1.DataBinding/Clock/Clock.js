9266a8a3930d1342989182adf7eac6dc311d0061
jain208 <abhi2000@gmail.com>
2014-03-03 16:07:32 +0000

    Added basic unit test

---
+++ ngBookDemos/ngBookDemos/App/1.DataBinding/Clock/Clock.js
@@ -0,0 +1,5 @@
+﻿var app = angular.module("clockApp", []);
+
+app.controller("ClockController", ["$scope", function ($scope) {
+    $scope.name = "Abhishek Jain";
+}]);
---
+++ ngBookDemos/ngBookDemos/Tests/1.DataBinding/ClockTests.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+describe("Clock", function () {
+    var scope, $controllerConstructor;
+
+    beforeEach(module("clockApp"));
+
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        $controllerConstructor = $controller;
+    }));
+    it("should set the name of the user", function () {
+        var mockEvents = {};
+        var ctrl = $controllerConstructor("ClockController", { $scope: scope });
+
+        expect(scope.name).toBe("Abhishek Jain");
+    });
+});
