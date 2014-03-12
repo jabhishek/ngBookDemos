9e5e28f2b8e929f53c0ba53a6d757d5c968cc744
Abhishek Jain <abhi2000@gmail.com>
2014-03-12 22:33:07 +0000

    Directives using isolated scope

---
+++ ngBookDemos/ngBookDemos/App/7_Directives/current-scope-intro.js
@@ -0,0 +1,16 @@
+﻿
+var app = angular.module("myApp", [])
+    .run(function($rootScope) {
+        $rootScope.rootProperty = "root value";
+    })
+    .controller("ParentController", ["$scope", function($scope) {
+        $scope.parentProperty = "parent property";
+    }])
+    .controller("ChildController", ["$scope", function ($scope) {
+        $scope.childProperty = "child property";
+        $scope.fullSentenceFromChild
+            = 'Same $scope: We can access: ' +
+                $scope.rootProperty + ' and ' +
+                $scope.parentProperty + ' and ' +
+                $scope.childProperty;
+    }]);
--- ngBookDemos/ngBookDemos/App/7_Directives/myDirective.js
+++ ngBookDemos/ngBookDemos/App/7_Directives/myDirective.js
@@ -5,4 +5,18 @@
             replace: true,
             template: '<div><a href="http://google.com" target="_blank">Click me to go to Google</a></div>'
         };
+    })
+    .directive("mySecondDirective", function ($log) {
+        return {
+            restrict: "A",
+            scope: {
+                myUrl: "@",
+                myText: "@"
+            },
+            replace: true,
+            template: '<div><a href=http://{{myUrl}} target="_blank">{{myText}}</a></div>',
+            controller: function($scope) {
+                $log.info($scope.myUrl);
+            }
+        };
     });
